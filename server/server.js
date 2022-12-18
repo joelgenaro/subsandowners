c7e0becc1a7ffed3f8986b2509e6349468e4fe2b
DESKTOP-P2LK7PT\senio <seniordeveloper754@gmail.com>
2022-12-17 22:55:39 -0500

    added * to path of the server.js

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(errorHandler);
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 // listen
