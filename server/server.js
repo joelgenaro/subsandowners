eafbdb4a894c70c66e5fdf9a9ba827f1405ea770
DESKTOP-P2LK7PT\senio <seniordeveloper754@gmail.com>
2022-12-17 22:04:56 -0500

    client build

--- server/server.js
+++ server/server.js
@@ -34,14 +34,11 @@ app.use("/api/jobList", jobList);
 
 app.use(errorHandler);
 
-if (process.env.NODE_ENV === "production") {
-  //*Set static folder up in production
-  app.use(express.static("./client/build"));
+app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
-}
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 // listen
 app.listen(PORT, () =>
   console.log(`Server is running on http://localhost:${PORT}`)
