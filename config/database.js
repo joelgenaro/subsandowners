8c44d64f11e977d483657ea259b5745d3b9835df
DESKTOP-P2LK7PT\senio <seniordeveloper754@gmail.com>
2022-12-17 19:43:30 -0500

    mongose.strict

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbURL = process.env.MONGO_DB_URL;
+mongoose.set("strictQuery", false);
 
 const configDatabase = async () => {
   try {
