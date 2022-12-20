f397fd62f4e06e6457b419dfaad5d7c56a8cf1c1
DESKTOP-P2LK7PT\senio <seniordeveloper754@gmail.com>
2022-12-20 07:54:01 -0500

    strictquery

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const dbURL = process.env.MONGO_DB_URL;
+mongoose.set("strictQuery", false);
 
 const configDatabase = async () => {
   try {
@@ -12,7 +13,7 @@ const configDatabase = async () => {
     });
     console.log("database connected");
   } catch (err) {
-    console.log("database issue=====>", err, "------------end----------------");
+    console.log("database issue=====>", err, "------------end---------------");
 
     process.exit(1);
   }
