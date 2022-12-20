8ca7489bfe58c9d70eb1b73f97d15fb0dfd39651
DESKTOP-P2LK7PT\senio <seniordeveloper754@gmail.com>
2022-12-20 13:46:54 -0500

    removed config

--- client/src/services/Auth/subcontractorService.js
+++ client/src/services/Auth/subcontractorService.js
@@ -12,19 +12,19 @@ const login = async (userData) => {
 
 // Forgot password
 const forgotPassword = async (email) => {
-  const response = await axios.post(`${API_URL}/forgotPassword`, email, config);
+  const response = await axios.post(`${API_URL}/forgotPassword`, email);
   return response.data;
 };
 
 // Register User
 const createAccountGmail = async (userData) => {
-  const response = await axios.post(`${API_URL}/add`, userData, config);
+  const response = await axios.post(`${API_URL}/add`, userData);
   return response.data;
 };
 
 // Update User profile
 const updateProfile = async (params) => {
-  const response = await axios.post(`${API_URL}/update`, params, config);
+  const response = await axios.post(`${API_URL}/update`, params);
   return response.data;
 };
 
