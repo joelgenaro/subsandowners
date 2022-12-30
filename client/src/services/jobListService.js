14a074f39dd7190fb3605f0d63777060b953f175
DESKTOP-P2LK7PT\senio <seniordeveloper754@gmail.com>
2022-12-30 12:49:37 -0500

    updated server url

--- client/src/services/jobListService.js
+++ client/src/services/jobListService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 
-const API_URL = `http://localhost:5000/api/jobList`;
+const API_URL = `https://subsandowners.onrender.com/api/jobList`;
 const config = {
   headers: {
     "Content-Type": "application/json",
