6934179a6b81cd34a853e1fd244b48aedb1ac51c
DESKTOP-P2LK7PT\senio <seniordeveloper754@gmail.com>
2023-05-20 09:15:59 -0400

    const userId = req.user["_id"];

--- server/controllers/cProfile.js
+++ server/controllers/cProfile.js
@@ -4,7 +4,7 @@ const Job = require("../models/mJob");
 
 const getProfile = async (req, res, next) => {
   try {
-    const userId = req.body.id != null ? req.body.id : req.user["_id"];
+    const userId = req.user["_id"];
     const profile = await User.findOne({ _id: userId });
     console.log(userId, profile);
     res.status(201).json({
